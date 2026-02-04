fa5fe2270f8030bd9320baee4cb0cc4f84aeb958
brunasantos <brunasantos@task.com.br>
2026-02-04 09:15:02 -0300

    Melhorando aparencia

--- components/Task/Settings/index.tsx
+++ components/Task/Settings/index.tsx
@@ -1,19 +1,22 @@
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
 export default function TaskSettings() {
     return (
         <View style={styles.settingsContainer}>
+            <TouchableOpacity style={styles.buttonGroup}>
+                <View style={styles.iconWrap}>
+                    <FontAwesome6
+                        name="group-arrows-rotate"
+                        size={24}
+                        color="black"
+                    />
+                </View>
+                <Text style={styles.text}>GRUPO</Text>
+            </TouchableOpacity>
             <View>
-                <FontAwesome6
-                    name="group-arrows-rotate"
-                    size={24}
-                    color="black"
-                />
-                <Text>GRUPO</Text>
-            </View>
-            <View>
+                {/* has to be a FlatList */}
                 <Text>Manhã</Text>
             </View>
         </View>
--- components/Task/Settings/styles.ts
+++ components/Task/Settings/styles.ts
@@ -2,14 +2,37 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     settingsContainer: {
-        height: 150,
+        height: 100,
         // margin: 12,
-        padding: 15,
+        padding: 10,
         backgroundColor: "#B7C5FE",
         borderRadius: 10,
         width: "40%",
         alignItems: "center",
 
         marginBottom: 10,
+    },
+    buttonGroup: {
+        borderBottomColor: "#7D8BF7",
+        borderBottomWidth: 1,
+        paddingBottom: 8,
+        marginBottom: 10,
+        width: "90%",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-around",
+    },
+    iconWrap: {
+        justifyContent: "center",
+        alignItems: "center",
+        width: 35,
+        height: 35,
+        borderRadius: 20,
+        backgroundColor: "#7D8BF7",
+    },
+    text: {
+        marginLeft: 8,
+        fontWeight: "bold",
+        fontSize: 20,
     },
 });
